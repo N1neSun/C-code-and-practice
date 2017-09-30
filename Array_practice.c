327ff10d934a341d33a74e8e879634cc658a778e
N1neSun <917549681@qq.com>
2017-09-30 10:16:23 +0800

    C语言入门第十二篇，数组练习题

---
+++ Array_practice.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main()
+{
+	int i,j;
+	int a[3][4];
+	for(i=0;i<3;i++)
+	{
+		for(j=0;j<4;j++)
+		{
+			scanf("%d",&a[i][j]);
+		}
+	}
+	for(i=0;i<3;i++)
+	{
+		for(j=0;j<4;j++)
+		{
+			printf("%d\t",a[i][j]);
+		}
+		printf("\n");
+	}
+	return 0;
+}
